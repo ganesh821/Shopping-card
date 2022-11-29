bcb2a97e255b0eca8978aeb9dcb49ffb7bbbb8a2
ganesh.r <ganesh.r@ilearningengines.com>
2022-11-29 15:30:31 +0530

    My second issue find.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -75,7 +75,7 @@ useEffect(() => {
             <span className="title">Subtotal ({cart.length}) items</span>
             <span style={{ fontWeight: 700, fontSize: 20 }}>Total: ₹ {total}</span>
         <Button type="button" disabled={cart.length === 0}>
-          Proceed to Checkout
+          Proceed to Checkout.
         </Button>
       </div>
     </div>
